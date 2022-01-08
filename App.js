74cf117968284bfdc76000e99005b33353bc9a1e
magali <magalipuuujol@gmail.com>
2022-01-08 17:50:13 -0300

    hamburger menu

--- App.js
+++ App.js
@@ -13,7 +13,17 @@ app.component("custom-btn", {
 app.component("custom-link", {
   props: { text: String, reference: String },
   template:
-    '<a class="custom-link" :href="reference" target="_blank">{{ text }}</a>',
+    '<a class="custom-link" :href="reference" target="_blank">{{ text }}</a><br><br>',
 });
 
+app.component("custom-nav", {
+    data() {
+        return {links: [
+          { text: 'texto', link: 'https://open.spotify.com/' },
+          { text: 'textoo', link: 'https://open.spotify.com/' },
+          { text: 'textooo', link: 'https://open.spotify.com/' }]
+    }},
+  template:
+    '<nav role="navigation"><div id="menuToggle"><input type="checkbox" /><span></span><span></span> <span></span> <ul id="menu"><custom-link v-for="link in links" :text="link.text" :reference="link.link"></custom-link></ul></div></nav>',
+});
 app.mount("#app");
