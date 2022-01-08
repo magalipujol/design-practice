8aebdd38677e7607ca651dda8c6e75cb050276d7
magali <magalipuuujol@gmail.com>
2022-01-08 18:17:14 -0300

    title style and component

--- App.js
+++ App.js
@@ -21,9 +21,17 @@ app.component("custom-nav", {
         return {links: [
           { text: 'texto', link: 'https://open.spotify.com/' },
           { text: 'textoo', link: 'https://open.spotify.com/' },
+          { text: 'textoo', link: 'https://open.spotify.com/' },
+          { text: 'textoo', link: 'https://open.spotify.com/' },
+          { text: 'textoo', link: 'https://open.spotify.com/' },
           { text: 'textooo', link: 'https://open.spotify.com/' }]
     }},
   template:
     '<nav role="navigation"><div id="menuToggle"><input type="checkbox" /><span></span><span></span> <span></span> <ul id="menu"><custom-link v-for="link in links" :text="link.text" :reference="link.link"></custom-link></ul></div></nav>',
 });
+
+app.component("custom-title", {
+    props: { text: String, emphasis: String },
+    template: '<h1 :class="emphasis"> {{ text }} </>'
+})
 app.mount("#app");
